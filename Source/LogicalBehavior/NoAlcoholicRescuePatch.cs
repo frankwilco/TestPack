e4139f0b0e1fb963d1c21713abe14994a7be0b16
Francis Dominic Fajardo <fajardo@duck.com>
2024-06-12 11:31:33 +0800

    Use a transpiler and fix up NARP for changes in RimWorld v1.5.

--- Source/LogicalBehavior/NoAlcoholicRescuePatch.cs
+++ Source/LogicalBehavior/NoAlcoholicRescuePatch.cs
@@ -1,26 +1,76 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Reflection.Emit;
 using Verse;
 
 namespace FrankWilco.RimWorld
 {
     [HarmonyPatch(typeof(HealthAIUtility),
-        nameof(HealthAIUtility.WantsToBeRescuedIfDowned))]
+        nameof(HealthAIUtility.WantsToBeRescued))]
     public static class NoAlcoholicRescuePatch
     {
-        public static bool Prefix(Pawn pawn, ref bool __result)
+        private static bool IsDrunk(Pawn pawn)
         {
-            var alcoholHediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.AlcoholHigh);
+            bool alcoholHediff = pawn.health.hediffSet.HasHediff(
+                HediffDefOf.AlcoholHigh);
 
-            if (alcoholHediff != null
+            if (alcoholHediff
                 && !pawn.health.HasHediffsNeedingTendByPlayer()
                 && pawn.health.hediffSet.BleedRateTotal <= 0.01f)
             {
-                __result = false;
-                return false;
+                return true;
             }
 
-            return true;
+            return false;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(
+            ILGenerator generator,
+            IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var alwaysDownedSection = new ILSection(
+                codes,
+                -1,
+                4,
+                OpCodes.Call,
+                (object operand) => ((MethodInfo)operand).Name == "AlwaysDowned",
+                OpCodes.Ldarg_0,
+                OpCodes.Ret);
+#if DEBUG
+            Log.Warning($"[TP] Was NAR patch applied: {alwaysDownedSection.IsFound}");
+#endif
+            if (alwaysDownedSection.IsFound)
+            {
+                Label alwaysDownedCheckArgPushLabel = generator.DefineLabel();
+                var isDrunkCheck = new Collection<CodeInstruction>()
+                {
+                    // We reuse the load argument instruction from the
+                    // insertion point.
+                    CodeInstruction.Call(
+                        typeof(NoAlcoholicRescuePatch),
+                        nameof(IsDrunk)),
+                    // if (IsDrunk(pawn)) {}
+                    new CodeInstruction(OpCodes.Brfalse_S,
+                        alwaysDownedCheckArgPushLabel),
+                    new CodeInstruction(OpCodes.Ldc_I4_0),
+                    // return false;
+                    new CodeInstruction(OpCodes.Ret),
+                    // Create a new load argument instruction for the always
+                    // downed conditional check.
+                    new CodeInstruction(OpCodes.Ldarg_0)
+                    {
+                        labels = { alwaysDownedCheckArgPushLabel }
+                    },
+                };
+                codes.InsertRange(
+                    alwaysDownedSection.StartIndex + 1,
+                    isDrunkCheck);
+            }
+            return codes;
         }
     }
 }
